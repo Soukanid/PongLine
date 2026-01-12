cf35e39b04e49d9039db868a2378ddb11cafe350
BBOT Fatimezzahra <fz.bbot@gmail.com>
2026-01-12 18:01:26 +0100

    fixed dependencies

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,13 @@
-import './style.css'; // If you have CSS
-import { Router } from './core/Router';
+import "./style.css"; // If you have CSS
+import { Router } from "./core/Router";
 
 // Import your pages
-import './features/auth/LoginPage';
+import "./features/auth/LoginPage";
 
 const routes = {
-  '/': 'page-login',
-  '/game': 'page-game',
-  '/chat': 'page-chat'
+  "/": "page-login",
+  "/game": "page-game",
+  "/chat": "page-chat",
 };
 
 new Router(routes);
